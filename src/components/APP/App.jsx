9f9ed2fc11c6300ae3f8b74e604aa49072d011d5
Daria Khanina <khanina_dasha@ukr.net>
2024-01-13 15:44:06 +0200

    COMMIT 2

---
+++ src/components/APP/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Feedback from 'components/Feedback/Feedback';
+
+
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0
+//   }
+// }
+
+
+export const App = () => {
+  <>
+    <h1>Please leave feeadback</h1>
+
+    <Feedback/> 
+
+    <h1>Statistics</h1>
+  </>
+  };
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
